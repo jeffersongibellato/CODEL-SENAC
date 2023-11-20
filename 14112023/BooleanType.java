3d8a2dd6cc3a859a89766e08d2d9e61ec42fc920
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:10:46 -0300

    Update BooleanType.java

--- 14112023/BooleanType.java
+++ 14112023/BooleanType.java
@@ -4,5 +4,6 @@ public class BooleanType {
         boolean isFishTasty = false;    
         System.out.println(isJavaFun);
         System.out.println(isFishTasty);
+        // cria duas variaveis do tipo booleano e coloca verdadeiro para 'java é divertido' e falso para 'peixe não é bom'
     }
 }
