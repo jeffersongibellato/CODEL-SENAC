e4b4b8c407a2e555585171406e1815860f95cb15
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:45:57 -0300

    Update LongType.java

--- 14112023/LongType.java
+++ 14112023/LongType.java
@@ -2,5 +2,6 @@ public class LongType {
     public static void main(String[] args) {
         long myNum = 15000000000L;
         System.out.println(myNum);  
+        // cria uma variavel do tipo long e printa na tela
       }
 }
