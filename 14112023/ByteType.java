6710fa78ded5ae09cf6aa6b481eb7fb2af5d26cd
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:11:30 -0300

    Update ByteType.java

--- 14112023/ByteType.java
+++ 14112023/ByteType.java
@@ -2,5 +2,6 @@ public class ByteType {
     public static void main(String[] args) {
         byte myNum = 100;
         System.out.println(myNum);  
+        //cria uma variavel do tipo byte e mostra na tela o valor dela
     }
 }
