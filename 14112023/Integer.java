385acd5a7674c334375818a03aef86be2c8c5e8c
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:40:49 -0300

    Update Integer.java

--- 14112023/Integer.java
+++ 14112023/Integer.java
@@ -2,5 +2,6 @@ public class Integer {
   public static void main(String[] args) {
     int myNum = 15;
     System.out.println(myNum);
+    // cria uma variavel de tipo inteiro numérico e printa na tela
   }
 }
