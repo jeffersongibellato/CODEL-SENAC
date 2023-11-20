3cc510a6bec072591faf3399d0f4c14f81c03cc9
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:31:35 -0300

    Update CommaIntegers.java

--- 14112023/CommaIntegers.java
+++ 14112023/CommaIntegers.java
@@ -2,5 +2,6 @@ public class CommaIntegers {
     public static void main(String[] args) {
         int x = 5, y = 6, z = 50;
         System.out.println(x + y + z);
+        // cria 3 variaveis do tipo inteiro e para separar as declarações de variaveis é utilizado a virgula, depois disso é somado as 3 variaveis no print da tela
     }
 }
