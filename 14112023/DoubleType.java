cb43e86277f634fdedc0bb221f460d91d7602b2d
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:37:41 -0300

    Update DoubleType.java

--- 14112023/DoubleType.java
+++ 14112023/DoubleType.java
@@ -2,5 +2,6 @@ public class DoubleType {
     public static void main(String[] args) {
         double myNum = 19.99d;
         System.out.println(myNum);  
+        // cria uma variavel do tipo double e printa ele na tela
     }
 }
