d21cf588584c7910b8cbcbba2548cf215e2cc600
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:38:23 -0300

    Update FloatType.java

--- 14112023/FloatType.java
+++ 14112023/FloatType.java
@@ -2,5 +2,6 @@ public class FloatType {
     public static void main(String[] args) {
         float myNum = 5.75f;
         System.out.println(myNum);  
+        // declara uma variavel do tipo float e printa na tela
     }
 }
