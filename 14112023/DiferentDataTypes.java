227770315103f91f1ff1a23936ff5167dbfb9829
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:37:00 -0300

    Update DiferentDataTypes.java

--- 14112023/DiferentDataTypes.java
+++ 14112023/DiferentDataTypes.java
@@ -1,11 +1,11 @@
 public class DiferentDataTypes {
     public static void main(String[] args) {
-        int myNum = 5;               // integer (whole number)
-        float myFloatNum = 5.99f;    // floating point number
-        char myLetter = 'D';         // character
-        boolean myBool = true;       // boolean
-        String myText = "Hello";     // String    
-        System.out.println(myNum);
+        int myNum = 5;               // declara um número inteiro
+        float myFloatNum = 5.99f;    // declara um número com ponto flutuante
+        char myLetter = 'D';         // declara uma variavel do tipo char, armazena um caractere
+        boolean myBool = true;       // declara uma variavel do tipo booleano 
+        String myText = "Hello";     // declara uma variavel do tipo string para amarzenar um texto    
+        System.out.println(myNum);    // dessa linha em diante será apenas os prints na tela das variaveis criadas
         System.out.println(myFloatNum);
         System.out.println(myLetter);
         System.out.println(myBool);
