8b9ef05a00825c0dcd560a07f15a245e451abc9a
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 15:09:12 -0300

    Update MathAbsolute.java

--- 14112023/MathAbsolute.java
+++ 14112023/MathAbsolute.java
@@ -1,5 +1,6 @@
 public class MathAbsolute {
     public static void main(String[] args) {
         System.out.println(Math.abs(-4.7));  
+        // printa na tela o valor absoluto do valor inserido
       }
 }
