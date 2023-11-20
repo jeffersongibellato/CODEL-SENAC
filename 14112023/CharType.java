b5e657acfb025f7d7ade747497f8e68493b0363c
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:15:24 -0300

    Update CharType.java

--- 14112023/CharType.java
+++ 14112023/CharType.java
@@ -2,5 +2,6 @@ public class CharType {
     public static void main(String[] args) {
         char myGrade = 'B';
         System.out.println(myGrade);
+        //cria uma variavel do tipo char para armazenar apenas uma letra e printa na tela
     }
 }
