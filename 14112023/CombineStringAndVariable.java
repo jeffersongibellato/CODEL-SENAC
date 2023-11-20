bf5ebadcd8d858e2f73286b117e8e3f87fa50c2a
Jefferson Junior Gibellato <96699942+jeffersongibellato@users.noreply.github.com>
2023-11-20 14:17:04 -0300

    Update CombineStringAndVariable.java

--- 14112023/CombineStringAndVariable.java
+++ 14112023/CombineStringAndVariable.java
@@ -2,5 +2,6 @@ public class CombineStringAndVariable {
     public static void main(String[] args) {
         String name = "John";
         System.out.println("Hello " + name);
+        // cria uma variavel do tipo string para armazenar um nome e mostra na tela a saudação e o nome da variavel
     }
 }
